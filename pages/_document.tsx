5c2da898aaec0fd60cf29183bbba424fb55e8428
ShahFH <shahfaisal.swe@gmail.com>
2023-08-13 15:21:47 +0430

    fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'This is my personal blog',
+    description: 'MY personal blog using next.js.',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png',
   }
 
